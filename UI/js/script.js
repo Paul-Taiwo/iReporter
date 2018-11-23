632570cdd825c05942e67d5119865117d368b077
Paul Taiwo <pauladedokun2000@gmail.com>
2018-11-23 19:37:46 +0100

    Added responsiveness

--- UI/js/script.js
+++ UI/js/script.js
@@ -2,3 +2,13 @@
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
+//Menu toggle
+document.getElementById("toggler").addEventListener("click", function(e) {
+    e.preventDefault;
+    document.getElementById("side-menu").style.left="0";
+});
+
+document.getElementById("hideMenu").addEventListener("click", function(e) {
+    e.preventDefault;
+    document.getElementById("side-menu").style.left="-240px";
+});
