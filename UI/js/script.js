5e58e07b253e436f849aebb80223af73e55ada48
Paul Taiwo <pauladedokun2000@gmail.com>
2018-11-24 16:56:44 +0100

    Wrote a fuction to toggle between the tabs for form

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,15 +1,38 @@
 // Dropdown toggle
-    document.getElementById("toggleDropdown").addEventListener("click", function(e) {
-        document.getElementById("myDropdown").classList.toggle("show");
+    document.querySelector("#toggleDropdown").addEventListener("click", function(e) {
+        document.querySelector("#myDropdown").classList.toggle("show");
     });
 
 //Menu toggle
-document.getElementById("toggler").addEventListener("click", function(e) {
+document.querySelector("#toggler").addEventListener("click", function(e) {
     e.preventDefault;
-    document.getElementById("side-menu").style.left="0";
+    document.querySelector("#side-menu").style.left="0";
 });
 
-document.getElementById("hideMenu").addEventListener("click", function(e) {
+document.querySelector("#hideMenu").addEventListener("click", function(e) {
     e.preventDefault;
-    document.getElementById("side-menu").style.left="-240px";
+    document.querySelector("#side-menu").style.left="-240px";
 });
+
+//Create Red flag record tab
+function openForm(evnt, contentId) {
+    let tabcontent, tablinks;
+
+    // Get all the elements with the class "content" and hide them
+    tabcontent = document.querySelectorAll(".tabcontent");
+    
+    tabcontent.forEach(function(item){
+        item.style.display="none";
+    });
+
+    // Get all the elements with the class "tablinks" and remove class active
+    tablinks = document.querySelectorAll(".tablinks");
+
+    tablinks.forEach(function(item){
+        item.className = item.className.replace(" active", "");
+    });
+
+    // Show the current tab and add an active class to it
+    document.querySelector(contentId).style.display="block";
+    evnt.currentTarget.className += " active";
+}
