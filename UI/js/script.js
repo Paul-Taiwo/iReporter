8b35d3fa3930a14458705788d5f2d9fa778d999b
Paul Taiwo <pauladedokun2000@gmail.com>
2018-11-25 15:15:39 +0100

    Updated

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,34 +1,46 @@
 // Dropdown toggle
-    document.querySelector("#toggleDropdown").addEventListener("click", function(e) {
+    document.querySelector("#toggleDropdown").addEventListener("click", (e) => {
+        e.preventDefault;
         document.querySelector("#myDropdown").classList.toggle("show");
     });
 
 //Menu toggle
-document.querySelector("#toggler").addEventListener("click", function(e) {
+document.querySelector("#toggler").addEventListener("click", (e) => {
     e.preventDefault;
     document.querySelector("#side-menu").style.left="0";
 });
 
-document.querySelector("#hideMenu").addEventListener("click", function(e) {
+document.querySelector("#hideMenu").addEventListener("click", (e) => {
     e.preventDefault;
     document.querySelector("#side-menu").style.left="-240px";
 });
 
+// Toggle side menu active class
+const changeActiveLink = (evnt, linkId) => {
+    let sideLinks = document.querySelectorAll("#side-list li a");
+
+    sideLinks.forEach(item => {
+        item.classList = item.classList.replace(" active", "");
+
+        evnt.currentTarget.className += "active";
+    });
+}
+
 //Create Red flag record tab
-function openForm(evnt, contentId) {
+const openForm = (evnt, contentId) => {
     let tabcontent, tablinks;
 
     // Get all the elements with the class "content" and hide them
     tabcontent = document.querySelectorAll(".tabcontent");
     
-    tabcontent.forEach(function(item){
+    tabcontent.forEach(item => {
         item.style.display="none";
     });
 
     // Get all the elements with the class "tablinks" and remove class active
     tablinks = document.querySelectorAll(".tablinks");
 
-    tablinks.forEach(function(item){
+    tablinks.forEach(item => {
         item.className = item.className.replace(" active", "");
     });
 
