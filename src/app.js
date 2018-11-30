9d6d41f02af2f5a7062073cce1c58abe46b47e53
Paul Taiwo <pauladedokun2000@gmail.com>
2018-11-30 19:25:50 +0100

    Setup Updated [#162337896]

--- src/app.js
+++ src/app.js
@@ -6,9 +6,18 @@ import cors from 'cors';
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(cors);
+app.use(bodyParser.urlencoded({ extended: false, }));
+app.use(cors());
 app.use(logger('dev'));
 
+app.get('/', (req, res) => {
+    console.log('Hello World');
+    console.log('This is working now');
+    return res.status(200).json({
+        message: 'Hello World',
+    })
+})
 
-app.listen(7071, () => console.log('App listening at port 7071'));
+app.listen(8080, () => {
+    console.log('Hey');
+})
