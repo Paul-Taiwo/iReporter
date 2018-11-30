3ed0d03157dd1b3dc9c067aed0fb72e340913daa
Paul Taiwo <pauladedokun2000@gmail.com>
2018-11-30 19:53:23 +0100

    Setup router [Finishes #162352805]

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.status(200).json({
+    status: 'success',
+    message: 'Hello World',
+}));
+
+export default app;
