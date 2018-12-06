10a0e09290c2121a0597ec1db21c96b0d22c207b
Paul Taiwo <pauladedokun2000@gmail.com>
2018-12-07 00:39:28 +0100

    feat(test, models, routes): Update red-flag location
    - add test for update location
    - add coveralls
    
    [Delivers #162493918]

--- src/routes/records.js
+++ src/routes/records.js
@@ -10,8 +10,7 @@ const Route = express.Router();
 Route.post('/', RecordValidations.createRecord, RecordController.createRecord);
 Route.get('/', RecordController.getAll);
 Route.get('/:id', RecordValidations.checkId, RecordController.getOne);
-Route.patch('/:id', RecordValidations.createRecord, RecordController.update);
-Route.patch('/:id/:location', RecordController.updateLocation);
+Route.patch('/:id/location', RecordValidations.createRecord, RecordController.updateLocation);
 Route.delete('/:id', RecordController.delete);
 
 Route.get('/:id');
