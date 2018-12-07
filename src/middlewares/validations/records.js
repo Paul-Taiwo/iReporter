710d8a25d85831f12aa322e14eac7f0b7fe89836
Paul Taiwo <pauladedokun2000@gmail.com>
2018-12-07 16:31:20 +0100

    chore(validation): Remove validation
    - remove validation for createdBy
    
    [Finishes #162363006]

--- src/middlewares/validations/records.js
+++ src/middlewares/validations/records.js
@@ -5,7 +5,7 @@ const { Record } = models;
 class RecordsValidate {
   static createRecord(req, res, next) {
     const {
-      name, type, location, createdBy, comment,
+      name, location, comment,
     } = req.body;
 
     if (!name || name.length < 1) {
@@ -15,24 +15,17 @@ class RecordsValidate {
       });
     }
 
-    if (!type) {
-      return res.status(400).json({
-        status: 400,
-        error: 'Please provide a valid record type',
-      });
-    }
+    // if (!type) {
+    //   return res.status(400).json({
+    //     status: 400,
+    //     error: 'Please provide a valid record type',
+    //   });
+    // }
 
     if (!location || location.length < 2) {
       return res.status(400).json({
         status: 400,
         error: 'Please provide a valid location',
-      });
-    }
-
-    if (!createdBy || createdBy.length < 2) {
-      return res.status(400).json({
-        status: 400,
-        error: 'Please provide your name',
       });
     }
 
