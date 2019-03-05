f5425ad9581f6801ef0ffab2af29a84c0b64107c
Paul Taiwo <pauladedokun2000@gmail.com>
2019-03-05 16:11:32 +0100

    Add more validations

--- src/usingDB/middlewares/validator.js
+++ src/usingDB/middlewares/validator.js
@@ -1,17 +1,52 @@
 
 class Validate {
   static validate(req, res, next) {
-    const { firstname, lastname, email, password } = req.body;
+    const {
+      firstname,
+      lastname,
+      othernames,
+      email,
+      password,
+      username,
+    } = req.body;
 
-    if (!firstname || firstname.trim().length <= 2 || !lastname || lastname.trim().length <= 2 || (email === undefined || email.trim().length < 1) || (password === undefined || password.trim().length < 1)) {
-      return res.status(400).json({
-        status: 400,
-        error: 'Provide a valid name',
-      });
-    }
+    (() => {
+      switch (true) {
+        case !firstname || !lastname || !othernames:
+          return res.status(400).json({
+            status: 400,
+            message: 'Name fieldss cannot be empty',
+          });
+        case firstname.trim().length <= 2 || lastname.trim().length <= 2 || othernames.trim().length <= 2:
+          return res.status(400).json({
+            status: 400,
+            message: 'Name fields cannot be less than two characters',
+          });
+        case !username:
+          return res.status(400).json({
+            status: 400,
+            message: 'Please input a username',
+          });
+        case !email:
+          return res.status(400).json({
+            status: 400,
+            message: 'Please input an email',
+          });
+        case !password:
+          return res.status(400).json({
+            status: 400,
+            message: 'Password field cannot be empty',
+          });
+        case password.trim().length < 8:
+          return res.status(400).json({
+            status: 400,
+            message: 'Password cannot be less than 8 characters',
+          });
 
-
-
+        default:
+          break;
+      }
+    })();
     return next();
   }
 }
