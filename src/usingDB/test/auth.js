499e547910711e27ef34b101573dcb06a2c46bc6
Paul Taiwo <pauladedokun2000@gmail.com>
2019-03-05 19:21:12 +0100

    chore(test): Setup test for auth

---
+++ src/usingDB/test/auth.js
@@ -0,0 +1,233 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import { describe, it } from 'mocha';
+import app from '../../app';
+
+chai.use(chaiHttp);
+
+describe('Testing Validations', () => {
+  it('Should return an error if firstname is empty', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: '',
+        lastname: 'Adedokun',
+        othernames: 'Taiwo',
+        phoneNumber: '',
+        username: 'latty',
+        email: 'lattyy@gmail.com',
+        password: 'abcdefh',
+      })
+      .end((err, res) => {
+        expect(err).to.equal(null);
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.message).to.equal('Name fields cannot be empty');
+        done();
+      });
+  });
+
+  it('Should return an error if lastname is empty', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: 'Paul',
+        lastname: '',
+        othernames: 'Taiwo',
+        phoneNumber: '',
+        username: 'latty',
+        email: 'lattyy@gmail.com',
+        password: 'abcdefh',
+      })
+      .end((err, res) => {
+        expect(err).to.equal(null);
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.message).to.equal('Name fields cannot be empty');
+        done();
+      });
+  });
+
+  it('Should return an error if othernames is empty', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: 'Paul',
+        lastname: 'Adedokun',
+        othernames: '',
+        phoneNumber: '',
+        username: 'latty',
+        email: 'lattyy@gmail.com',
+        password: 'abcdefh',
+      })
+      .end((err, res) => {
+        expect(err).to.equal(null);
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.message).to.equal('Name fields cannot be empty');
+        done();
+      });
+  });
+
+  it('Should return an error if firstname is less than 2 characters', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: 'P',
+        lastname: 'Adedokun',
+        othernames: 'Taiwo',
+        phoneNumber: '',
+        username: 'latty',
+        email: 'lattyy@gmail.com',
+        password: 'abcdefh',
+      })
+      .end((err, res) => {
+        expect(err).to.equal(null);
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.message).to.equal('Name fields cannot be less than two characters');
+        done();
+      });
+  });
+
+  it('Should return an error if lastname is less than 2 characters', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: 'Paul',
+        lastname: 'A',
+        othernames: 'Taiwo',
+        phoneNumber: '',
+        username: 'latty',
+        email: 'lattyy@gmail.com',
+        password: 'abcdefh',
+      })
+      .end((err, res) => {
+        expect(err).to.equal(null);
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.message).to.equal('Name fields cannot be less than two characters');
+        done();
+      });
+  });
+
+  it('Should return an error if othernames is less than 2 characters', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: 'Paul',
+        lastname: 'Adedokun',
+        othernames: 'T',
+        phoneNumber: '',
+        username: 'latty',
+        email: 'lattyy@gmail.com',
+        password: 'abcdefh',
+      })
+      .end((err, res) => {
+        expect(err).to.equal(null);
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.message).to.equal('Name fields cannot be less than two characters');
+        done();
+      });
+  });
+
+  it('Should return an error if username is empty', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: 'Paul',
+        lastname: 'Adedokun',
+        othernames: 'Taiwo',
+        phoneNumber: '',
+        username: '',
+        email: 'lattyy@gmail.com',
+        password: 'abcdefh',
+      })
+      .end((err, res) => {
+        expect(err).to.equal(null);
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.message).to.equal('Please input a username');
+        done();
+      });
+  });
+
+  it('Should return an error if password is empty', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: 'Paul',
+        lastname: 'Adedokun',
+        othernames: 'Taiwo',
+        phoneNumber: '',
+        username: 'latty',
+        email: 'lattyy@gmail.com',
+        password: '',
+      })
+      .end((err, res) => {
+        expect(err).to.equal(null);
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.message).to.equal('Password field cannot be empty');
+        done();
+      });
+  });
+
+  it('Should return an error if passsword is less than 8 characters', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/auth/signup')
+      .set({
+        'Content-type': 'application/json',
+      })
+      .send({
+        firstname: 'Paul',
+        lastname: 'Adedokun',
+        othernames: 'Taiwo',
+        phoneNumber: '',
+        username: 'latty',
+        email: 'lattyy@gmail.com',
+        password: 'abcde',
+      })
+      .end((err, res) => {
+        expect(err).to.equal(null);
+        expect(res.statusCode).to.equal(400);
+        expect(res.body.status).to.equal(400);
+        expect(res.body.message).to.equal('Password cannot be less than 8 characters');
+        done();
+      });
+  });
+});
